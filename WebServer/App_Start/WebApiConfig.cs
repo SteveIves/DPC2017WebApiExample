d974b216a534642cb3b2ce761a925d4f201a731d
Steve Ives <steve.ives@synergex.com>
2017-05-10 14:03:58 -0400

    Changes

--- WebServer/App_Start/WebApiConfig.cs
+++ WebServer/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using WebApiServices;
+using WebApiServices.Filters;
 
 namespace WebServer
 {
@@ -10,6 +12,12 @@ namespace WebServer
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            //Implement global model state validation
+            config.Filters.Add(new ValidateModelAttribute());
+
+            //Implement a Unity IoC container
+            config.DependencyResolver = WebApiServicesConfig.GetUnityResolver();
+
 
             // Web API routes
             config.MapHttpAttributeRoutes();
